6b48dcfdcce2d5a7895753745c90cfaa5093cef3
andrés <acastroaraujo@gmail.com>
2024-07-04 12:17:10 -0400

    update

--- app.R
+++ app.R
@@ -3,39 +3,32 @@ library(shiny)
 library(actsims)
 library(actdata)
 library(dplyr)
+library(purrr)
 library(tidyr) ## only used in dictionary_reactive()
 library(bslib)
 library(shinyjs)
 library(plotly)
 library(DT)
 library(histoslider)
-#source("utils.R")
+
+
+# To do:
+# - Customize, print stuff from Aidan's package.
+# - "Add 3d plot with observers, and add closest terms panel. Use an observer
+#   so that the plot doesn't restart all the time"
+#   https://linking.plotly-r.com/linking-views-with-shiny#fig:shiny-scatterplot
 
 available_equations <- actdata::equations |> 
   dplyr::filter(equation_type == "impressionabo") |> 
   dplyr::select(key, group)
 
-available_dictionaries <- purrr::map(actdata::get_dicts(), \(x) x@groups)
+available_dictionaries <- purrr::map(actdata::get_dicts(), \(x) sort(x@groups))
 names(available_dictionaries) <- purrr::map_chr(actdata::get_dicts(), \(x) x@key)
 
 ok <- purrr::map(actdata::get_dicts(), \(x) x@components) |> 
   purrr::map_lgl(\(x) all(c("identity", "behavior") %in% x))
 
 available_dictionaries <- available_dictionaries[ok]
-
-
-# Customize, print stuff from Aidan's package!
-
-# sometimes the dots have a weird %{text} bold name. This happens when there's
-# only one left.
-
-# Make code that decides which dictionaries are worthy of inclusion—i.e., that 
-# they have at least identity and behavior components.
-
-# "Add 3d plot with observers, and add closest terms panel. Use an observer
-# so that the plot doesn't restart all the time"
-
-# https://linking.plotly-r.com/linking-views-with-shiny#fig:shiny-scatterplot
 
 # dictionary --------------------------------------------------------------
 
@@ -113,10 +106,16 @@ ui_analyze_events <- bslib::card(
       shiny::selectizeInput("init_object", "Person 2 (Object)", choices = NULL),
       shiny::verbatimTextOutput("init_object_epa")
     ),
-    bslib::layout_columns(
-      col_widths = c(4, 8),
-      bslib::card("Controls", shiny::br(), shiny::helpText("add grey-out optimal behavior and reidentification panels with closest words to optimal in the bottom")),
-      bslib::card("Main Output", shiny::br(), shiny::helpText("add history panel with deflection scores, possibility of looking at metadata, and visualization"))
+    bslib::layout_column_wrap(
+      width = 1/2,
+      bslib::card(
+        title = "Controls",
+        uiOutput("new_event")
+      ),
+      bslib::navset_card_underline(
+        bslib::nav_panel("Deflection", DT::dataTableOutput("history_deflection")),
+        bslib::nav_panel("Visualization", plotly::plotlyOutput("vizzz"))
+      )
     )
   )
 )
@@ -132,7 +131,7 @@ ui_customize <- bslib::card(
       shiny::selectizeInput("equations", "Equations", choices = unique(available_equations$key), selected = "us2010"),
       shiny::selectizeInput("equations_subset", "Group", choices = "all")
     ),
-    bslib::card("Info")
+    bslib::card()
   )
 )
 
@@ -140,9 +139,6 @@ ui_customize <- bslib::card(
 ui <- bslib::page_navbar(
   shinyjs::useShinyjs(),
   title = "Minimalist Interact",
-  
- # theme = bslib::bs_theme() |> 
- #   bs_add_rules(list(sass::sass_file("styles.css"))),
   
   nav_panel(title = "Dictionary", ui_dictionary),
   nav_panel(title = "Solve", ui_solve),
@@ -154,8 +150,7 @@ ui <- bslib::page_navbar(
 
 server <- function(input, output, session) {
   
-  ## Customize ---------------------------------------------------------------
-  
+  # Customize -------------------------------------------------------------------
   
   observeEvent(input$dictionary, {
     freezeReactiveValue(input, "dictionary_subset")
@@ -170,6 +165,7 @@ server <- function(input, output, session) {
   
   ## This is the most important line [!]
   ACT <- reactive({
+    ## add a validation function here
     interact(dictionary = list(input$dictionary, input$dictionary_subset), equations = list(input$equations, input$equations_subset))
   }) 
   
@@ -193,10 +189,13 @@ server <- function(input, output, session) {
 
   ## Analyze Events ----------------------------------------------------------
 
-  observeEvent(list(identities(), behaviors()), {
-    updateSelectizeInput(session, 'init_actor', choices = identities(), selected = sample(identities(), 1), server = TRUE)
-    updateSelectizeInput(session, 'init_behavior', choices = behaviors(), selected = sample(behaviors(), 1), server = TRUE)
-    updateSelectizeInput(session, 'init_object', choices = identities(), selected = sample(identities(), 1), server = TRUE)
+  observeEvent(behaviors(), {
+    shiny::updateSelectizeInput(session, 'init_behavior', choices = behaviors(), selected = sample(behaviors(), 1), server = TRUE)
+  })
+  
+  observeEvent(identities(), {
+    shiny::updateSelectizeInput(session, 'init_actor', choices = identities(), selected = sample(identities(), 1), server = TRUE)
+    shiny::updateSelectizeInput(session, 'init_object', choices = identities(), selected = sample(identities(), 1), server = TRUE)
   })
   
   output$init_deflection <- renderUI({
@@ -245,21 +244,22 @@ server <- function(input, output, session) {
     shinyjs::disable("init_actor")
     shinyjs::disable("init_behavior")
     shinyjs::disable("init_object")
-    updateActionButton(session, "init_button", disabled = TRUE, icon = icon("circle-pause"))
-    updateActionButton(session, "init_restart", disabled = FALSE)
+    shinyjs::enable("add_event")
+    shiny::updateActionButton(session, "init_button", disabled = TRUE, icon = icon("circle-pause"))
+    shiny::updateActionButton(session, "init_restart", disabled = FALSE)
   })
   
   observeEvent(input$init_restart, {
     shinyjs::enable("init_actor")
     shinyjs::enable("init_behavior")
     shinyjs::enable("init_object")
-    updateActionButton(session, "init_button", disabled = FALSE, icon = icon("play"))
-    updateActionButton(session, "init_restart", disabled = TRUE)
+    shinyjs::disable("add_event")
+    shiny::updateActionButton(session, "init_button", disabled = FALSE, icon = icon("play"))
+    shiny::updateActionButton(session, "init_restart", disabled = TRUE)
   })
   
-  # HERE situation ---------- 
   
-  situation <- reactive({
+  situation <- eventReactive(input$init_button, {
     req(input$init_actor)
     req(input$init_behavior)
     req(input$init_object)
@@ -269,6 +269,255 @@ server <- function(input, output, session) {
       dictionary = list(input$dictionary, input$dictionary_subset),
       equations = list(input$equations, input$equations_subset)
     )
+  })
+  
+  event_counter <- reactiveVal(NULL) ## this seems
+  observeEvent(input$init_button, event_counter(0L)) ## too
+  observeEvent(input$add_event, event_counter(event_counter() + 1L)) ## hacky
+  
+  history <- eventReactive(event_counter(), {
+    
+    if (input$who == "person1" && event_counter() > 0) {
+      message("works")
+      situation()$next_person1(list(A = input$new_a, B = input$new_b, O = input$new_o))
+    }
+    
+    if (input$who == "person2" && event_counter() > 0) {
+      situation()$next_person2(list(A = input$new_a, B = input$new_b, O = input$new_o))
+    }
+    
+    situation()$history
+    
+  })
+  
+  output$history_deflection <- DT::renderDataTable({
+    
+    history()$deflection |> 
+      dplyr::select(time, deflection, dplyr::everything()) |> 
+      DT::datatable(
+        rownames = FALSE,
+        options = list(
+          paging = FALSE,
+          scrollY = 500,
+          scroller = TRUE
+        )
+      ) |> 
+      DT::formatRound(columns = c("deflection"), digits = 2)
+    
+  })
+  
+  output$history_fundamentals <- DT::renderDataTable({
+    col_select <- c(actsims:::epa_selector("A"), actsims:::epa_selector("B"), actsims:::epa_selector("O"))
+    
+    DT::datatable(
+      data = history()$fundamentals[c("time", col_select)],
+      rownames = FALSE,
+      options = list(
+        paging = FALSE,
+        scrollY = 500,
+        scroller = TRUE
+      )
+    ) |> 
+    DT::formatRound(columns = col_select, digits = 2)
+    
+  })
+  
+  output$history_transients <- DT::renderDataTable({
+    col_select <- c(actsims:::epa_selector("A"), actsims:::epa_selector("B"), actsims:::epa_selector("O"))
+    
+    DT::datatable(
+      data = history()$transients[c("time", col_select)],
+      rownames = FALSE,
+      options = list(
+        paging = FALSE,
+        scrollY = 500,
+        scroller = TRUE
+      )
+    ) |> 
+    DT::formatRound(columns = col_select, digits = 2)
+    
+  })
+  
+  output$history_element_wise_deflections <- DT::renderDataTable({
+    col_select <- c(actsims:::epa_selector("A"), actsims:::epa_selector("B"), actsims:::epa_selector("O"))
+    
+    DT::datatable(
+      data = history()$element_wise_deflection[c("time", col_select)],
+      rownames = FALSE,
+      options = list(
+        paging = FALSE,
+        scrollY = 500,
+        scroller = TRUE
+      )
+    ) |> 
+    DT::formatRound(columns = col_select, digits = 2)
+    
+  })
+  
+  output$new_event <- renderUI({
+    
+    if (input$init_button == 0) {
+      validate("Press the initialize button!")
+    }
+    
+    req(situation())
+    
+    opt_beh <- situation()$optimal_behavior("person1")
+    opt_act <- situation()$reidentify("person1")
+    opt_obj <- situation()$reidentify("person1")
+    
+    closest_beh <- situation()$engine$closest_terms(opt_beh, "behavior", max_dist = 3)
+    closest_act <- situation()$engine$closest_terms(opt_act, "identity", max_dist = 3)
+    closest_obj <- situation()$engine$closest_terms(opt_obj, "identity", max_dist = 3)
+    
+    row1 <- shiny::fluidRow(
+      column(6, radioButtons("who", "Actor:", choices = c("Person 1" = "person1", "Person 2" = "person2"), selected = c("Person 2" = "person2"), inline = TRUE)),
+      column(6, actionButton("add_event", "Add event"))
+    )
+    
+    row2 <- shiny::fluidRow(
+      column(6, shiny::selectizeInput("new_a", shiny::helpText("Actor"), choices = union(input$init_actor, names(closest_act)))),
+      column(6, helpText("Optimal Reidentification"), br(), shiny::verbatimTextOutput("new_a_opt"))
+    )
+    
+    row3 <- shiny::fluidRow(
+      column(6, shiny::selectizeInput("new_b", shiny::helpText("Behavior"), choices = names(closest_beh))),
+      column(6, helpText("Optimal Behavior"), br(), shiny::verbatimTextOutput("new_b_opt"))
+    )
+    
+    row4 <- shiny::fluidRow(
+      column(6, shiny::selectizeInput("new_o", shiny::helpText("Object"), choices = union(input$init_object, names(closest_obj)))),
+      column(6, helpText("Optimal Reidentification"), br(), shiny::verbatimTextOutput("new_o_opt"))
+    )
+    
+    bslib::navset_card_underline(
+      bslib::nav_panel(
+        "Add Event", 
+        row1, 
+        row2,
+        div(verbatimTextOutput("closest_actor"), style = "height: 100px; overflow-y: scroll; border: 1px solid #ccc;"),
+        row3,
+        div(verbatimTextOutput("closest_behavior"), style = "height: 100px; overflow-y: scroll; border: 1px solid #ccc;"),
+        row4,
+        div(verbatimTextOutput("closest_object"), style = "height: 100px; overflow-y: scroll; border: 1px solid #ccc;")
+      ),
+      bslib::nav_panel("Fundamentals", DT::dataTableOutput("history_fundamentals")),
+      bslib::nav_panel("Transients", DT::dataTableOutput("history_transients")),
+      bslib::nav_panel("Element Deflection", DT::dataTableOutput("history_element_wise_deflections"))
+    )
+    
+    
+  })
+  
+  output$new_a_opt <- renderPrint({
+    opt_act <- situation()$reidentify(input$who)
+    round(unlist(opt_act), 3)
+  })
+  
+  output$closest_actor <- renderPrint({
+    
+    opt_act <- situation()$reidentify(input$who)
+    out <- situation()$engine$closest_terms(opt_act, "identity", max_dist = 3)
+    data.frame(distance = out, row.names = names(out))
+    
+    
+  })
+  
+  output$new_b_opt <- renderPrint({
+    opt_beh <- situation()$optimal_behavior(input$who)
+    round(unlist(opt_beh), 3)
+  })
+  
+  output$closest_behavior <- renderPrint({
+    opt_beh <- situation()$optimal_behavior(input$who)
+    out <- situation()$engine$closest_terms(opt_beh, "behavior", max_dist = 3)
+    data.frame(distance = out, row.names = names(out))
+  })
+  
+  output$new_o_opt <- renderPrint({
+    opt_obj <- switch(input$who,
+      "person1" = situation()$reidentify("person2"),
+      "person2" = situation()$reidentify("person1")
+    )
+    round(unlist(opt_obj), 3)
+  })
+  
+  output$closest_object <- renderPrint({
+    opt_obj <- switch(input$who,
+      "person1" = situation()$reidentify("person2"),
+      "person2" = situation()$reidentify("person1")
+    )
+    out <- situation()$engine$closest_terms(opt_obj, "identity", max_dist = 3)
+    data.frame(distance = out, row.names = names(out))
+  })
+  
+  observeEvent(input$add_event, {
+    person_choices <- c("Person 1" = "person1", "Person 2" = "person2")
+    
+    current_person <- switch(input$who,
+      "person1" = "Person 2",
+      "person2" = "Person 1"
+    )
+    
+    shiny::updateRadioButtons(session, "who", choices = person_choices, selected = person_choices[current_person])
+  })
+
+  
+  # update selectInputs based on input$who
+  observeEvent(input$who, {
+    
+    # optimal behaviors/ re-identifications
+    
+    opt_beh <- situation()$optimal_behavior(input$who)
+    opt_act <- situation()$reidentify(input$who)
+    opt_obj <- switch(input$who,
+      "person1" = situation()$reidentify("person2"),
+      "person2" = situation()$reidentify("person1")
+    )
+    
+    # closest terms
+    
+    closest_beh <- situation()$engine$closest_terms(opt_beh, "behavior", max_dist = 3)
+    closest_act <- situation()$engine$closest_terms(opt_act, "identity", max_dist = 3)
+    closest_obj <- situation()$engine$closest_terms(opt_obj, "identity", max_dist = 3)
+    
+    # current identities
+    
+    current <- tail(situation()$history$deflection, n = 1)
+    
+    actor <- switch(current[[input$who]],
+      "actor" = current$A,
+      "object" = current$O
+    )
+    
+    object <- switch(current[[input$who]],
+      "actor" = current$O,
+      "object" = current$A
+    )
+    
+    shiny::updateSelectizeInput(session, 'new_b', choices = names(closest_beh), server = TRUE)
+    shiny::updateSelectizeInput(session, 'new_a', choices = union(actor, names(closest_act)), server = TRUE)
+    shiny::updateSelectizeInput(session, 'new_o', choices = union(object, names(closest_obj)), server = TRUE)
+    
+  })
+  
+  output$vizzz <- plotly::renderPlotly({
+    
+    d <- history()$deflection
+    A_who <- ifelse(d$person1 == "actor", "Person 1", "Person 2")
+    O_who <- ifelse(d$person1 == "object", "Person1", "Person 2")
+    
+    tooltip <- with(d, paste(paste0(A, " (", A_who, ")"), B, paste0(O, " (", O_who, ")"), sep = ", "))
+    
+    d$time <- as.factor(d$time)
+    
+    plot_ly(
+      data = d,
+      x = ~time, y = ~deflection, mode = "lines+markers", type = "scatter", text = I(tooltip),
+      hovertemplate = paste("%{text}<br><br>", "Deflection:", "%{y}", "<extra></extra>")
+      ) |>
+    layout(yaxis = list(hoverformat = '.2f'))
+    
   })
 
   
@@ -377,7 +626,7 @@ server <- function(input, output, session) {
       max_dist = input$solve_max_dist
     )
     
-    data.frame(distance = out)
+    data.frame(distance = out, row.names = names(out))
     
   })
   
@@ -396,9 +645,9 @@ server <- function(input, output, session) {
     epa <- do.call(rbind, ACT()$dictionary$ratings)
 
     tagList(
-      histoslider::input_histoslider("filter_e", label = "Evaluation", values = epa[, "e"], height = 150),
-      histoslider::input_histoslider("filter_p", label = "Potency", values = epa[, "p"], height = 150),
-      histoslider::input_histoslider("filter_a", label = "Activity", values = epa[, "a"], height = 150),
+      histoslider::input_histoslider("filter_e", label = "Evaluation", values = epa[, "e"], height = 150, start = -4.3, end = 4.3),
+      histoslider::input_histoslider("filter_p", label = "Potency", values = epa[, "p"], height = 150, start = -4.3, end = 4.3),
+      histoslider::input_histoslider("filter_a", label = "Activity", values = epa[, "a"], height = 150, start = -4.3, end = 4.3),
       checkboxGroupInput("filter_components", label = "Components", choiceNames = purrr::map(component_choices, helpText), choiceValues = component_choices, inline = TRUE, selected = component_choices)
     )
     
@@ -432,7 +681,7 @@ server <- function(input, output, session) {
       mode = "markers",
       x = ~e, y = ~a, z = ~p, 
       color = ~component,
-      text = ~term,
+      text = ~I(term),
       hovertemplate = paste(
         "<b>%{text}</b><br><br>",
         "E: %{x} <br>",
